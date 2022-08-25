01b2d86b5ad5affd3c646f31bbed1a13950085ad
Vineel <vineeln@gmail.com>
2022-08-25 12:12:16 -0400

    v0.1.0 create urql client in _app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -14,23 +14,17 @@ import { __GRAPHQL_API_SERVER__ } from '../app/app-constants';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
 
-  // const cacheEx = cacheExchange(urqlConfig.caccheExchangeConfig);
-  // const client = createClient({
-  //   url: __GRAPHQL_API_SERVER__,
-  //   //requestPolicy:'network-only',
-  //   fetchOptions: {
-  //     credentials: "include",
-  //   },
-  //   exchanges: [devtoolsExchange, dedupExchange, cacheEx, fetchExchange],
-  // });
-
+  const client = createClient(urqlConfig.UrqlClientConfig())
   console.log("loading _app");
+  
   return (
+    <Provider value={client}>
       <AuthProvider>
         <ChakraProvider theme={theme}>
           <Component {...pageProps} />
         </ChakraProvider>
       </AuthProvider>
+    </Provider>
   )
 }
 
